10c427ed45cae630ad19e326f9a78fb51d2c1d52
Francisco Jimenez <J7_samuslive.dr@hotmail.com>
2016-10-31 02:01:58 -0600

    Agregando variables locales. Este es el proyecto de reddit empezado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'reddit',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  //title = 'app works!';
+  addArticle(title: HTMLInputElement,link: HTMLInputElement): boolean {
+  	console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  	return false;
+  }
+  /*Typescript: Como decir en javascript: 
+  addArticle(title,link){
+	console.log('Adding article title: '+title.value+' and link: '+link.value);
+	return false;
+  }
+  Esta función se identifica como una función con retorno de tipo booleano. 
+  Notese la sintaxis de la declaración de la función y la declaración de las cadenas usando el acento invertido: `
+  */
 }
+
+
